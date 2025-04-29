2d9539b23e9cb9f39217478f8b246f24ca8335bc
kanuriharshini2001 <harshinikanuri2001@gmail.com>
2025-04-29 12:42:30 -0400

    Carbon_credits_SE

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.android_uber"
+    namespace = "com.example.androiduber"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
